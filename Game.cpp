8d724de1936144dc731e15eadc18403d477d0e93
vhparikh <42686619+vhparikh@users.noreply.github.com>
2020-09-25 09:15:31 -0700

    comments for game files

--- Game.cpp
+++ Game.cpp
@@ -1,21 +1,26 @@
+//Created by Vatsal Parikh
+//Date 9/24/20
+//cpp file for game class
+
+//imports
 #include <iostream>
 #include <cstring>
 #include "Game.h"
 
 using namespace std;
 
-Game::Game() {
+Game::Game() { //constructor
 
 }
 
-char* Game::getPublisher() {
+char* Game::getPublisher() { //returns pointer to publisher
   return publisher;
 }
 
-float* Game::getRating() {
+float* Game::getRating() { //returns memory location rating
   return &rating;
 }
 
-int Game::getType() {
+int Game::getType() { //returns type of object
   return 0;
 }
--- Game.h
+++ Game.h
@@ -1,16 +1,21 @@
+//Created by Vatsal Parikh
+//Date 9/24/20
+//Header for Game class
+
+//imports
 #include <iostream>
 #include <cstring>
 #include "Media.h"
 
 using namespace std;
 
-class Game : public Media {
-public:
+class Game : public Media { //Game class extends Media
+public: //Public constructor and methods
   Game();
   char* getPublisher();
   float* getRating();
   virtual int getType();
-private:
+private: //private variables
   char publisher[100];
   float rating;
 };
