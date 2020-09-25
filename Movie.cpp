0f1991d08335bce741edfea050a0d29b58041815
vhparikh <42686619+vhparikh@users.noreply.github.com>
2020-09-25 09:41:00 -0700

    comments for movie

--- Movie.cpp
+++ Movie.cpp
@@ -1,25 +1,30 @@
+//Created by Vatsal Parikh
+//Date 9/24/20
+//cpp file for movie class
+
+//imports
 #include <iostream>
 #include <cstring>
 #include "Movie.h"
 
 using namespace std;
 
-Movie::Movie() {
+Movie::Movie() { //constructor for movie
 
 }
 
-char* Movie::getDirector() {
+char* Movie::getDirector() { //returns pointer to director
   return director;
 }
 
-int* Movie::getDuration() {
+int* Movie::getDuration() { //returns memory location of duration
   return &duration;
 }
 
-float* Movie::getRating() {
+float* Movie::getRating() { //returns memory location of rating
   return &rating;
 }
 
-int Movie::getType() {
+int Movie::getType() { //returns type of object
   return 2;
 }
--- Movie.h
+++ Movie.h
@@ -1,17 +1,22 @@
+//Created by Vatsal Parikh
+//Date 9/24/20
+//header file for movie class
+
+//imports
 #include <iostream>
 #include <cstring>
 #include "Media.h"
 
 using namespace std;
 
-class Movie : public Media {
-public:
+class Movie : public Media { //movie class extends media class
+public: //public constructor and methods
   Movie();
   char* getDirector();
   int* getDuration();
   float* getRating();
   virtual int getType();
-private:
+private: //private variables
   char director[100];
   int duration;
   float rating;
