c0e00dce78f39f5c69935a8b27a07d3489bd220e
vhparikh <42686619+vhparikh@users.noreply.github.com>
2020-09-24 20:16:38 -0700

    Media Comments

--- Media.cpp
+++ Media.cpp
@@ -1,21 +1,26 @@
+//Created by Vatsal Parikh
+//Date 9/24/20
+//cpp file for media class
+
+//imports
 #include <iostream>
 #include <cstring>
 #include "Media.h"
 
 using namespace std;
 
-Media::Media() {
+Media::Media() { //constructor
 
 }
 
-char* Media::getTitle() {
+char* Media::getTitle() { //returns title pointer
   return title;
 }
 
-int* Media::getYear() {
+int* Media::getYear() { //returns address of year
   return &year;
 }
 
-int Media::getType() {
+int Media::getType() { //returns type of file
   return -1;
 }
--- Media.h
+++ Media.h
@@ -1,18 +1,24 @@
+//Created by Vatsal Parikh
+//Date 9/24/20
+//header file for Media class
+
+//imports
 #include <iostream>
 #include <cstring>
 
+//header guard
 #ifndef MEDIA_H
 #define MEDIA_H
 
 using namespace std;
 
 class Media {
-public:
+public: //public methods and constructors
   Media();
   char* getTitle();
   int* getYear();
   virtual int getType();
-private:
+private: //private variables
   char title[100];
   int year;
 };
